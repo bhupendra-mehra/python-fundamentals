2a4548e2b66e406de5a61213ddea9611ba57baf8
bhupendra-mehra <84669906+bhupendra-mehra@users.noreply.github.com>
2026-07-11 18:07:27 +0530

    Add files via upload

---
+++ Lesson-6/user-information-card.py
@@ -0,0 +1,13 @@
+print("========== USER INFORMATION CARD ==========")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+profession = input("Enter your profession: ")
+city = input("Enter your city: ")
+
+print("\n========== PROFILE ==========")
+print("Name       :", name)
+print("Age        :", age)
+print("Profession :", profession)
+print("City       :", city)
+print("================================")
