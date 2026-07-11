77980f3591a057886175eb9fa78d1f9efdf10a07
bhupendra-mehra <84669906+bhupendra-mehra@users.noreply.github.com>
2026-07-11 16:49:45 +0530

    Add files via upload

---
+++ Lesson-5/app.py
@@ -0,0 +1,53 @@
+# Print data in python
+# print("Welcome to AI Engineering")
+# print("My name is Bhupendra")
+# print("I am learning AI Agents")
+
+# Add variable in python
+# name = "Bhupendra"
+# role = "Magento Developer"
+# experience = 8
+
+# print(name)
+# print(role)
+# print(experience)
+
+# Add input in python
+# name = input("Enter your name: ")
+
+# print(name)
+
+# Exercise
+# print("Welcome to AI Learning")
+
+# name = input("Enter your name: ")
+
+# role = input("Enter your profession: ")
+
+# print("Hello", name)
+
+# print("Profession:", role)
+
+# print("Welcome to AI Engineering")
+
+
+print("Welcome to AI Learning")
+
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+role = input("Enter your profession: ")
+
+print("---------------------")
+
+print("Name:", name)
+
+print("Age:", age)
+
+print("Profession:", role)
+
+print("---------------------")
+
+print("Thank you for learning AI!")
+
