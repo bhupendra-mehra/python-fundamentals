a1c11e5443df30d4f99bbedb8447cbb1dd604967
bhupendra-mehra <84669906+bhupendra-mehra@users.noreply.github.com>
2026-07-11 16:49:20 +0530

    Add files via upload

---
+++ Lesson-6/lesson-6-conversion.py
@@ -0,0 +1,5 @@
+print("Age Calculator")
+
+age = int(input("Enter your age: "))
+
+print("Next year you will be:", age + 1)
---
+++ Lesson-6/lesson-6.py
@@ -0,0 +1,14 @@
+name = "Bhupendra"
+age = 37
+salary = 50000.50
+is_magento_developer = True
+
+print(name)
+print(age)
+print(salary)
+print(is_magento_developer)
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_magento_developer))
